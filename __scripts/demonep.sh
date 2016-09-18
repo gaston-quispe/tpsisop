bfa0917ce4e3a619f78317e5d8d4aaae0fe9a208
Seva Gavrilov <gavrilovseva@gmail.com>
2016-09-18 16:45:06 -0300

    Agregado un mock de funcionamiendo de logep y demonep.

---
+++ __scripts/demonep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ******************************************************************
+# Universidad de Buenos Aires
+# Facultad de Ingenieria
+#
+# 75.08 Sistemas Operativos
+# Catedra Ing. Osvaldo Clua
+#
+# Autores: Grupo 8
+#
+# ******************************************************************
+
+echo "Soy un demonio!";
+sleep 5;
---
+++ __scripts/logep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ******************************************************************
+# Universidad de Buenos Aires
+# Facultad de Ingenieria
+#
+# 75.08 Sistemas Operativos
+# Catedra Ing. Osvaldo Clua
+#
+# Autores: Grupo 8
+#
+# ******************************************************************
+
+
+# Esta función por ahora solo hace echo de lo que le pasan
+echo '>>>' $1;
