cda70d21996b3f0a528f2f60aab250eb9dd86a96
rubenJim <rbnm.jimenez@gmail.com>
2016-10-05 00:18:54 -0300

    movep terminado, falta ver detalle de loguear cuando lo usa instalep

--- __scripts/movep.sh
+++ __scripts/movep.sh
@@ -20,6 +20,7 @@ function grabarLog {
 
 
 #$1 archivo con path completo
+#$2 comando invocador
 function obtenerSecuencia {	
 	pathArchivo=$1	
 	dirArchivo=$(dirname $pathArchivo)/
@@ -31,7 +32,8 @@ function obtenerSecuencia {
 	
 	if [ $cantArchivos -gt $maxCantArchDupl ];then
 		#loguear Error
-		rango=3 #pongo cualquier cosa para que no rompa el if
+		grabarLog "$2" "no se pudo mover porque el archivo $1 supero la secuencia maxima permitida de 999" "WAR"
+		archivo=""
 	else
 		archivo=$archivo.$cantArchivos
 		archivo=$dirArchivo$archivo
@@ -57,10 +59,10 @@ function main {
 	#	exit -1
 	#fi
 
-	comandos=(Demonep Listep Initep Procep)
+	comandos=(demonep listep initep procep)
 
 	if [ -z "$3" ];then
-		comando=Movep
+		comando=movep
 	else
 		for i in ${comandos[*]}
 		do
@@ -68,32 +70,30 @@ function main {
 				comando=$3
 				break
 			else
-				comando=Movep
+				comando=movep
 			fi
 		done
 	fi
-	
-	echo "____________"
-	echo $comando
 
 
 	if ! [ -f $archivo ];then
 		#loguear mensaje
-#		grabarLog "$comando" "no se pudo mover debido a que : $archivo no es un archivo " "WAR"
-		echo "no existe el archivo"
+		grabarLog "$comando" "no se pudo mover debido a que : $archivo no es un archivo " "WAR"
 		exit -2	
 	fi
 
+
 	if ! [ -d $dirDestino ];then
-		echo "no existe el directorio"	
-#		grabarLog "$comando" "no se pudo mover debido a que : $dirDestino no es un directorio " "WAR"
+		grabarLog "$comando" "no se pudo mover debido a que : $dirDestino no es un directorio " "WAR"
 		#loguear mensaje
 		exit -3
 	fi
 
+
 	if  ! [[ $dirDestino  =~ $terminacionBarra ]];then
 		dirDestino=$dirDestino/	
 	fi
+
 
 	empiezaDelHome="^/home/.*/$"
 	path=${PWD}
@@ -126,29 +126,31 @@ function main {
 
 
 	if [ "$dirDestino" == "$dirOrigen" ]; then
-		echo "directorios iguales"
-#		grabarLog "$comando" "no se pudo mover debido a que : el directorio destino y el directorio origen son el mismo " "WAR"
+		grabarLog "$comando" "no se pudo mover debido a que : el directorio destino y el directorio origen son el mismo " "WAR"
 		#loguer mensaje
 		exit -4	
 	fi
 
 
 	if ! [ -f $archivoDestino ];then
-		echo "pasa que no existe duplicado y se mueve normalmente el archivo"
-#		grabarLog "$comando" "el archivo $archivo se movio satisfactoriamente a $dirDestino " "INFO"
+		grabarLog "$comando" "el archivo $archivo se movio satisfactoriamente a $dirDestino " "INFO"
 		mv $archivo $archivoDestino				
 	else
 		if ! [ -d $dirDestinoDuplicado ];then
 			mkdir $dirDestinoDuplicado
-			echo "se crea el directorio duplicado"
 		fi
 
-		archivoDestinoDuplicado=$(obtenerSecuencia $archivoDestinoDuplicado)
-#		grabarLog "$comando" "el archivo se encuentra duplicado en $dirDestino, y se movio a $archivoDestinoDuplicado" "INFO"
-		echo "-----------------"
-		echo "archivo destino  $archivoDestinoDuplicado"
-		echo "archivo origen $archivo"
-		echo "-----------------"
+		archivoDestinoDuplicado=$(obtenerSecuencia $archivoDestinoDuplicado $comando)
+		
+		if [ -z $archivoDestinoDuplicado ];then
+			exit -5
+		fi
+
+		grabarLog "$comando" "el archivo se encuentra duplicado en $dirDestino, y se movio a $dirDestinoDuplicado" "INFO"
+		#echo "-----------------"
+		#echo "archivo destino  $archivoDestinoDuplicado"
+		#echo "archivo origen $archivo"
+		#echo "-----------------"
 		mv $archivo $archivoDestinoDuplicado
 	fi
 
