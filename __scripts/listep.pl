3a0c2a485cae4b5c2157b44046fd39d4a79b2ee4
Seva Gavrilov <gavrilovseva@gmail.com>
2016-10-10 13:12:01 -0300

    Algunas mejoras al lsitado del presupuesto ejecutado.

--- __scripts/listep.pl
+++ __scripts/listep.pl
@@ -10,7 +10,7 @@
 #                  *
 # ******************************************************************
 
-# use strict;
+use strict;
 
 # Definiciones
 
@@ -19,6 +19,8 @@ my $dirmae;
 my $centros;
 my $anio;
 my $sancionado;
+# opción ct, si es true ordena primero por trimestre y luego por código de centro
+my $porTrimestre;
 
 # listado Presupuesto Sancionado
 sub listadoPS;
@@ -36,8 +38,8 @@ sub main() {
   $anio = '2016';
   $centros = $dirmae . '/centros.csv';
   $sancionado = $dirmae . '/sancionado-' . $anio . '.csv';
-  $tc = 0;
-  listadoPS(tc);
+  $porTrimestre = 0;
+  listadoPS();
 }
 
 main();
@@ -45,6 +47,7 @@ main();
 # Lee el archivo de centros y llena el diccionario centrosHash
 # con id_centro => nombre del centro
 sub readCentros {
+  my $centrosHashRef = shift;
 
   open(my $centrosFile, "<$centros") || die "ERROR: No puedo abrir el archivo $centros\n";
 
@@ -53,29 +56,51 @@ sub readCentros {
   while (my $line = <$centrosFile>) {
     chomp $line;
     my @fields = split(/;/, $line);
-    $centrosHash{$fields[0]} = $fields[1];
+    $centrosHashRef->{$fields[0]} = $fields[1];
   }
 
   close($centrosFile);
 }
 
-sub listadoPS {
-  my $tc = $_[0];
-  local %centrosHash;
 
-  readCentros();
+# Esta subrutina convierte las comas de una string a puntos, es necesario
+# para tratar números con punto flotante correctamente, ya que en los csvs
+# aparecen con comas.
+sub commaToDot {
+  my($value) = @_;
+  $value =~ s/\,/./g;
+  return $value;
+}
+
+sub dotToComma {
+  my($value) = @_;
+  $value = sprintf("%.2f", $value);
+  $value =~ s/\./,/g;
+  return $value;
+}
+
+sub listadoPS {
+  my %centrosHash;
+
+  readCentros(\%centrosHash);
 
   open(my $sancionadoFile, "<$sancionado") || die "ERROR: No puedo abrir el archivo $sancionado\n";
 
-  print "A\ño presupuestario $anio,Total Sancionado\n";
+  print "A\ño presupuestario $anio;Total Sancionado\n";
   # Ignoro el header
   my $header = <$sancionadoFile>;
-  %data;
+  my %data;
 
+  # En este while voy a llenar un hash de arrays de la siguiente forma
+  # En el caso de la opción ct (porTrimestre = true):
+  #   { Direccion General Alfa => [ [Primer Trimestre 2016, 121], [Segundo Trimestre 2016, 141], etc ]}
+  # En el caso de la opción tc (porTrimestre = false):
+  #   { Primer Trimestre 2016 => [ [Unidad Ministro, 2000], [Direccion General Alfa, 121], etc ]}
   while (my $line = <$sancionadoFile>) {
     my @fields = split(/;/, $line);
     my $centro = $centrosHash{$fields[0]};
-    if (tc) {
+    my $firstKey; my $secondKey;
+    if ($porTrimestre) {
       $firstKey = $centro; $secondKey = $fields[1];
     } else {
       $firstKey = $fields[1]; $secondKey = $centro;
@@ -84,22 +109,22 @@ sub listadoPS {
     if (!exists $data{$firstKey}) {
       $data{$firstKey} = [];
     }
-    push(@{$data{$firstKey}}, [$secondKey, $fields[2] + $fields[3]]);
+    my $value = commaToDot($fields[2]) + commaToDot($fields[3]);
+    push(@{$data{$firstKey}}, [$secondKey, $value]);
   }
 
   my $total = 0;
-  foreach my $key (keys %data) {
+  foreach my $key (sort keys %data) {
     my $subtotal = 0;
     foreach my $items ( @{$data{$key}} ) {
       my $value = @$items[1];
       $subtotal += $value;
-      print "@$items[0],$value\n";
+      print "@$items[0];" . dotToComma($value) . "\n";
     }
     $total += $subtotal;
-    print "$key,$subtotal\n";
+    print "$key;" . dotToComma($subtotal) . "\n";
   }
-  print "Total Anual,$total\n";
+  print "Total Anual" . dotToComma($total) . "\n";
 
-  my %centrosHash;
   close($sancionadoFile);
 }
